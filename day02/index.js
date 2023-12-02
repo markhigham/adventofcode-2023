ccf763b018604146f5a8b8cacf520987857e38c0
Mark Higham <mark.higham@gmail.com>
2023-12-02 17:49:50 +0000

    day02 wip

--- day02/index.js
+++ day02/index.js
@@ -1,5 +1,28 @@
 const readFileFromStdIn = require('../common/fileReader')
 
+function tokenize(word) {
+  const result = []
+
+  // Todo - loop through the digits in the word for
+  // digits or one, two ... nine
+  let buffer = word
+
+  while (buffer.length) {
+    result.push(buffer[0])
+    buffer = buffer.slice(1)
+  }
+
+  return result
+}
+
 readFileFromStdIn()
-  .then((lines) => console.log(lines))
+  .then((lines) => {
+    const total = lines.reduce((accumulator, line) => {
+      const tokens = tokenize(line)
+      console.log(tokens)
+      return accumulator
+    }, 0)
+
+    console.log(total)
+  })
   .catch((err) => console.error(err))
